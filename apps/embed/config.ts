c3aa862516f0ca2482125bb458ad69828a30a41f
Indra M <indra.meguga@ag-it.com>
2026-01-17 23:25:08 +0700

    34: embed script

---
+++ apps/embed/config.ts
@@ -0,0 +1,5 @@
+export const EMBED_CONFIG = {
+    WIDGET_URL: import.meta.env.VITE_WIDGET_URL || "http://localhost:3001",
+    DEFAULT_ORGANIZATION_ID: "org_38MuoNKILQVWKImf91WgcVZNJTA",
+    DEFAULT_POSITION: "bottom-right" as const,
+}
